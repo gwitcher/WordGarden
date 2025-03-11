c23a0d9efae27b9fb419d0fba5ada3eba4b247e5
Gabe Witcher <gabewitcher@gmail.com>
2025-03-11 13:53:43 -0700

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Gabe Witcher on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
